4b8a198a94c8053d8058ed3006de335e0dabbb86
JustAmirka <97117667+JustAmirka@users.noreply.github.com>
2023-04-26 12:36:12 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/babies/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.babies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/drawer.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'icons/icons.dart';
+
+
+
+
+Widget getDrawer(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  double screenHeight = MediaQuery.of(context).size.height;
+  return (Drawer(
+      child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+        getSeparateDivider(),
+        SizedBox(height: screenHeight*0.08,),
+
+        const Icon(Icons.person_pin,
+          size: 100,
+          color: Colors.lightBlue,),
+        Padding(
+          padding: EdgeInsets.only(left: screenWidth * 0.2),
+          child: const Text('Имя Фамилия',style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+            fontFamily: 'Roboto',
+            fontSize: 18.0,
+          ),
+          ),
+        ),
+        SizedBox(height: screenHeight*0.05,),
+        MaterialButton(
+            highlightColor: Colors.blue[800],
+            splashColor: Colors.blue[800],
+            child: Row(children: <Widget>[
+              const Icon(Icons.home_outlined,
+                color: Colors.lightBlue,),
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth * 0.1),
+                child: const Text(
+                  'Главное меню',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontFamily: 'Roboto',
+                    fontSize: 18.0,
+                  ),
+                ),
+              )
+            ]),
+            /*onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+            }*/),
+        SizedBox(height: screenHeight*0.02,),
+        MaterialButton(
+            highlightColor: Colors.blue[800],
+            splashColor: Colors.blue[800],
+            child: Row(children: <Widget>[
+              const Icon(Icons.person_2_outlined,
+                color: Colors.lightBlue,),
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth * 0.1),
+                child: const Text(
+                  'Мой профиль',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontFamily: 'Roboto',
+                    fontSize: 18.0,
+                  ),
+                ),
+              )
+            ]),
+            /*onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage()));
+            }*/),
+        SizedBox(height: screenHeight*0.02,),
+        MaterialButton(
+            highlightColor: Colors.blue[800],
+            splashColor: Colors.blue[800],
+            child: Row(children: <Widget>[
+              const Icon(Icons.map_outlined,
+                  color: Colors.lightBlue),
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth * 0.1),
+                child: const Text(
+                  'Маршруты',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontFamily: 'Roboto',
+                    fontSize: 18.0,
+                  ),
+                ),
+              )
+            ]),
+            /*onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => BusPage()));
+            }*/),
+        SizedBox(height: screenHeight*0.02,),
+        MaterialButton(
+            highlightColor: Colors.blue[800],
+            splashColor: Colors.blue[800],
+            child: Row(children: <Widget>[
+              const Icon(Icons.newspaper_outlined,
+                  color: Colors.lightBlue),
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth * 0.1),
+                child: const Text(
+                  'Новости',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontFamily: 'Roboto',
+                    fontSize: 18.0,
+                  ),
+                ),
+              )
+            ]),
+            /*onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => NewsPage()));
+            }*/),
+
+        SizedBox(height: screenHeight*0.02,),
+        MaterialButton(
+            highlightColor: Colors.blue[800],
+            splashColor: Colors.blue[800],
+            child: Row(children: <Widget>[
+              const Icon(Icons.medical_services_outlined,
+                  color: Colors.lightBlue),
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth * 0.1),
+                child: const Text(
+                  'Мед организации',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontFamily: 'Roboto',
+                    fontSize: 18.0,
+                  ),
+                ),
+              )
+            ]),
+            /*onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const ContactPage()));
+            }*/),
+
+
+        SizedBox(height: screenHeight*0.05,),
+        getSeparateDivider(),
+        MaterialButton(
+            highlightColor: Colors.blue[800],
+            splashColor: Colors.blue[800],
+            child: Row(children: <Widget>[
+              const Icon(Icons.settings_outlined,
+                  color: Colors.lightBlue),
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth * 0.1),
+                child: const Text(
+                  'Настройки',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontFamily: 'Roboto',
+                    fontSize: 18.0,
+                  ),
+                ),
+              )
+            ]),
+            /*onPressed: () {
+              Navigator.of(context).pop();
+            }*/),
+
+      ])));
+
+}
+class Drawhorizontalline extends CustomPainter {
+  late Paint _paint;
+  bool reverse;
+
+  Drawhorizontalline(this.reverse) {
+    _paint = Paint()
+      ..color = Colors.black26
+      ..strokeWidth = 2
+      ..strokeCap = StrokeCap.round;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawLine(const Offset(-180.0, 0.0), const Offset(150.0, 0.0), _paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+
+Widget getSeparateDivider() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      CustomPaint(painter: Drawhorizontalline(true)),
+    ],
+  );
+}
